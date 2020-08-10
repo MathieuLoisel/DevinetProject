f465291355d19fae05f872a7b31b2a6e81ed28bf
ludovicdean <ludovicd58@gmail.com>
2020-08-10 11:43:39 +0200

    Création des activités

---
+++ app/src/main/java/com/example/devinetproject/activity/MyResultsGeneralActivity.java
@@ -0,0 +1,16 @@
+package com.example.devinetproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.devinetproject.R;
+
+public class MyResultsGeneralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_results_general);
+    }
+}
---
+++ app/src/main/java/com/example/devinetproject/activity/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.example.devinetproject.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.devinetproject.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
