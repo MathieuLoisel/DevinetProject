345902f23304e943a15d5bcc9a21662e5d9baf33
Loisel <loiselmathieu35@gmail.Com>
2020-08-10 08:42:02 +0200

    fix return's type of get method for LiveData<List<Category>>

--- app/src/main/java/com/example/devinetproject/repository/category/CategoryRepository.java
+++ app/src/main/java/com/example/devinetproject/repository/category/CategoryRepository.java
@@ -1,12 +1,14 @@
 package com.example.devinetproject.repository.category;
 
+import androidx.lifecycle.LiveData;
+
 import com.example.devinetproject.bo.Category;
 
 import java.util.List;
 
 public interface CategoryRepository {
     void insert(Category ... categories);
-    List<Category> get();
+    LiveData<List<Category>> get();
     void update(Category ... categories);
     void delete(Category ... categories);
     void deleteAll();
