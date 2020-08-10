28a4577d421411891c4246a683806b5cdf4031a9
Loisel <loiselmathieu35@gmail.Com>
2020-08-10 08:34:06 +0200

    update CategoryRepository add methods

--- app/src/main/java/com/example/devinetproject/repository/category/CategoryRepository.java
+++ app/src/main/java/com/example/devinetproject/repository/category/CategoryRepository.java
@@ -1,4 +1,13 @@
 package com.example.devinetproject.repository.category;
 
+import com.example.devinetproject.bo.Category;
+
+import java.util.List;
+
 public interface CategoryRepository {
+    void insert(Category ... categories);
+    List<Category> get();
+    void update(Category ... categories);
+    void delete(Category ... categories);
+    void deleteAll();
 }
