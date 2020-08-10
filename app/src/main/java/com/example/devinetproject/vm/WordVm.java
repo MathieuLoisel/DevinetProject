302f1be490b7c50c90e4cbf37e2d2948f54a38b1
Loisel <loiselmathieu35@gmail.Com>
2020-08-10 09:51:54 +0200

    create WordVm OK

---
+++ app/src/main/java/com/example/devinetproject/vm/WordVm.java
@@ -0,0 +1,50 @@
+package com.example.devinetproject.vm;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.devinetproject.bo.Word;
+import com.example.devinetproject.repository.word.WordRepository;
+import com.example.devinetproject.repository.word.WordRepositoryImp;
+
+import java.util.List;
+
+/**
+ * Classe permettant la communication entre la vue (activity) et le repository
+ */
+public class WordVm extends AndroidViewModel {
+    /**
+     * Permet de n'avoir qu'un seul observateur pour toute l'application
+     */
+    private LiveData<List<Word>> observer;
+    private WordRepository wordRepository;
+
+    public WordVm(@NonNull Application application) {
+        super(application);
+        wordRepository = new WordRepositoryImp(application);
+        observer = wordRepository.get();
+    }
+
+    public LiveData<List<Word>> get(){
+        return observer;
+    }
+
+    public void insert(Word ... words){
+        wordRepository.insert(words);
+    }
+
+    public void update(Word ... words){
+        wordRepository.update(words);
+    }
+
+    public void delete(Word ... words){
+        wordRepository.delete(words);
+    }
+
+    public void deleteAll(){
+        wordRepository.deleteAll();
+    }
+}
