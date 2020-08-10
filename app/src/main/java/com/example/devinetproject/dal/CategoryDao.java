6d249c4ac68ae68f03d4d870824d2ca054a8ad33
Loisel <loiselmathieu35@gmail.Com>
2020-08-10 08:11:15 +0200

    create CategoryDao

---
+++ app/src/main/java/com/example/devinetproject/dal/CategoryDao.java
@@ -0,0 +1,38 @@
+package com.example.devinetproject.dal;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.devinetproject.bo.Category;
+import com.example.devinetproject.bo.Word;
+
+import java.util.List;
+
+/**
+ * Dao permettant de définir les fonctions d'accès à la BDD.
+ * Utilisation de Room en tant qu'ORM.
+ * Nous définissions les méthodes d'appel à la base de données et Room se chargera de les remplir
+ */
+
+@Dao
+public interface CategoryDao {
+    @Insert
+    void insert(Category... categories);
+
+    @Query("SELECT * FROM Category")
+    LiveData<List<Word>> get();
+
+    @Update
+    void update(Category ... categories);
+
+    @Delete
+    void delete(Category ... categories);
+
+    @Query("DELETE FROM Category")
+    void deleteAll();
+}
