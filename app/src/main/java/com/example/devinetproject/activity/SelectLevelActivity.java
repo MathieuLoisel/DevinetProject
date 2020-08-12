eead1305389612199642737dcd5a35de129b0bd1
ludovicdean <ludovicd58@gmail.com>
2020-08-12 10:26:40 +0200

    Merge branch 'master' of C:\Users\Administrateur\AndroidStudioProjects\DevinetProject with conflicts.

--- app/src/main/java/com/example/devinetproject/activity/SelectLevelActivity.java
+++ app/src/main/java/com/example/devinetproject/activity/SelectLevelActivity.java
@@ -1,16 +1,44 @@
 package com.example.devinetproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.devinetproject.R;
+import com.example.devinetproject.activity.adapter.LevelAdapter;
+import com.example.devinetproject.bo.Level;
+import com.example.devinetproject.vm.LevelVm;
+
+import java.util.List;
 
 public class SelectLevelActivity extends AppCompatActivity {
+
+    private LevelVm levelVM = null;
+    private ListView levelList = null;
+    private List<Level> levels = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_level);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        levelList = findViewById(R.id.lv_select_level);
+        levelVM = ViewModelProviders.of(this).get(LevelVm.class);
+
+        levelVM.get().observe(this, new Observer<List<Level>>() {
+            @Override
+            public void onChanged(List<Level> levels) {
+                SelectLevelActivity.this.levels = levels;
+                levelList.setAdapter(new LevelAdapter(SelectLevelActivity.this,R.layout.style_ligne_select_level_layout,levels));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/devinetproject/activity/adapter/LevelAdapter2.java
@@ -0,0 +1,4 @@
+package com.example.devinetproject.activity.adapter;
+
+public class LevelAdapter2 {
+}
