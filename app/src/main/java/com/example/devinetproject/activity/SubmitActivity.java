835d028dc78c08576aa1419112574d664c3259c0
Loisel <loiselmathieu35@gmail.Com>
2020-08-12 12:41:40 +0200

    add load and take photo impl code improvement

--- app/src/main/java/com/example/devinetproject/activity/SubmitActivity.java
+++ app/src/main/java/com/example/devinetproject/activity/SubmitActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,23 +12,24 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.devinetproject.R;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.util.Objects;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class SubmitActivity extends AppCompatActivity {
 
+    //Définition des codes REQUEST afin de déclencher les bons morceaux de codes en fonction du
+    // requestCode
     public static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final int RESULT_LOAD_IMAGE = 2;
-    public static final int TEST = 2;
-    Intent dataIntent;
+    public static final int RESULT_ALLOW_PERMISSION = 3;
+    private Intent dataIntent;
+    private ImageView imageSubmit = findViewById(R.id.iv_photo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,11 @@ public class SubmitActivity extends AppCompatActivity {
     public void onClickLastWord(View view) {
     }
 
+    /**
+     * Méthode permettant de prendre une photo avec l'appareil photo du téléphone et de l'afficher
+     * dans l'application
+     * @param view Vue liée à l'activité
+     */
     public void onClickTakePicture(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -50,62 +55,54 @@ public class SubmitActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Méthode permettant de charger une photo existante (depuis la gallery du téléphone) et de
+     * l'afficher dans l'application
+     * @param view Vue liée à l'activité
+     */
+    public void onCLickLoadPicture(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        );
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        ImageView imageSubmit = findViewById(R.id.iv_photo);
+
+        //Code activé après le onCLickLoadPicture
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+            Bitmap imageBitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             imageSubmit.setImageBitmap(imageBitmap);
         }
 
+        //Code activé après le onClickTakePicture
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
-            Log.i("test", "salut");
             dataIntent = data;
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, TEST);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    RESULT_ALLOW_PERMISSION
+            );
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == TEST && grantResults[0] == PERMISSION_GRANTED){
-            ImageView imageSubmit = findViewById(R.id.iv_photo);
+        if (requestCode == RESULT_ALLOW_PERMISSION && grantResults[0] == PERMISSION_GRANTED) {
             Uri selectedImage = dataIntent.getData();
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            assert selectedImage != null;
             Cursor cursor = getContentResolver().query(selectedImage,
                     filePathColumn, null, null, null);
+            assert cursor != null;
             cursor.moveToFirst();
             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
             String picturePath = cursor.getString(columnIndex);
-            Log.i("test", picturePath);
             Bitmap imageLoad = BitmapFactory.decodeFile(picturePath);
             imageSubmit.setImageBitmap(imageLoad);
             cursor.close();
-
         }
-
     }
-
-    public void onCLickLoadPicture(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, RESULT_LOAD_IMAGE);
-    }
-
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-
 }
